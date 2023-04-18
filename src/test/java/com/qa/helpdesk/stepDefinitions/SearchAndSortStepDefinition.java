aa43acb4c02b582c486013174ed6edd5ce4535af
Abhishek.Gautam <abhishek.gautam@geminisolutions.com>
2023-04-18 21:56:14 +0530

    scenarios added

--- src/test/java/com/qa/helpdesk/stepDefinitions/SearchAndSortStepDefinition.java
+++ src/test/java/com/qa/helpdesk/stepDefinitions/SearchAndSortStepDefinition.java
@@ -77,7 +77,7 @@ public class SearchAndSortStepDefinition {
                 DriverAction.waitUntilElementDisappear(DashboardHeaderLocators.loaderCover, 10);
             }
             List<String> values = new ArrayList<>();
-            DriverAction.scrollIntoView(TicketLocators.createTicket);
+            DriverAction.scrollIntoView(SearchAndSortLocators.columns(colName));
             DriverAction.waitUntilElementClickable(SearchAndSortLocators.columns(colName),10);
             DriverAction.click(SearchAndSortLocators.columns(colName),colName+" sort button");
             DriverAction.waitUntilElementClickable(TableAndPaginationLocators.paginationDropdown,5);
